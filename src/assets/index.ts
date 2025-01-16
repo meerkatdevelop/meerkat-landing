3862e966dc57ba6527f6091942773a6f00fb6a9c
culturalmass <herrera_manuel18@hotmail.com>
2025-01-16 14:01:49 +0100

    added:Portada

---
+++ src/assets/index.ts
@@ -0,0 +1,33 @@
+//Icons
+import ListIcon from './icons/list'
+import CaretDownIcon from './icons/caret-down'
+import CreditCardIcon from './icons/credit-card'
+import PlayIcon from './icons/play'
+//Images
+import Logo from './images/MEERKAT_LOGO 1.svg'
+import MeerkatCoin from './images/MEERKAT_COIN II.svg'
+import BnbLogo from './images/bnb-logo.svg'
+import Capa_1 from './images/CAPA_1.svg'
+//Flags
+import CountryEN1 from './flags/COUNTRY_EN 1.svg'
+//Coins
+import ETH from './coins/ethereum-eth.svg'
+import USDT from './coins/tether-usdt.svg'
+
+export {
+  //Icons
+  ListIcon,
+  CaretDownIcon,
+  CreditCardIcon,
+  PlayIcon,
+  //Images
+  Logo,
+  MeerkatCoin,
+  BnbLogo,
+  Capa_1,
+  //Flags
+  CountryEN1,
+  //Coins
+  ETH,
+  USDT,
+}
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface IconProps extends React.HTMLAttributes<HTMLDivElement> {
+  width?: string
+  height?: string
+  color?: string
+  className?: string
+}
