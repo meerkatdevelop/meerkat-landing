25eb60879131957458e140ce9cc9f2bca09ae6c7
javirrof <javirofe96@gmail.com>
2025-02-04 15:27:16 +0100

    Add structure for wallet connection

---
+++ src/contracts/Evm/chains.ts
@@ -0,0 +1,21 @@
+export const CHAINS_ONBOARD_CONFIG = {
+  ethereum: {
+    id: 1,
+    token: 'ETH',
+    label: 'Ethereum',
+    rpcUrl: 'https://mainnet.infura.io/v3/YourInfuraKey',
+  },
+
+  bsc: {
+    id: 56,
+    token: 'BNB',
+    label: 'Binance Smart Chain',
+    rpcUrl: 'https://bsc-dataseed.binance.org/',
+  },
+  base: {
+    id: 8453,
+    token: 'ETH',
+    label: 'Base',
+    rpcUrl: 'https://base.llamarpc.com',
+  },
+}
